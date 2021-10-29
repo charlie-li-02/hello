0633d286c34b2bb92b032045674dbc74768d2663
Charlie Li <charlie@jostle.me>
2021-10-28 17:22:52 -0700

    Updates

---
+++ precache-manifest.a244530d17113e041ae70a6be6b905c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc597abb02a7ed640f44d90bb9e5a20",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "61938e87346a4aa42c05",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "586b6f0567f90bb8dfdb",
+    "url": "/hello/static/js/2.dcc3031e.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/hello/static/js/2.dcc3031e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dec82a877737ebf15b",
+    "url": "/hello/static/js/3.0e80431b.chunk.js"
+  },
+  {
+    "revision": "61938e87346a4aa42c05",
+    "url": "/hello/static/js/main.25f10e18.chunk.js"
+  },
+  {
+    "revision": "406832f9a351c1cd2739",
+    "url": "/hello/static/js/runtime-main.44254268.js"
+  }
+]);
